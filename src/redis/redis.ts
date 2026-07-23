32586d5e65089be4d127356ad05eadb5ad1bbd74
Toxic209 <hey.itzrohit1@gmail.com>
2026-07-23 06:42:56 +0000

    feat: add temporary worker script in typescript for Redis stream processing and update nodemon configuration;

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -8,8 +8,8 @@ const redis = createClient({
     url: process.env.REDIS_URL as string
 });
 
-await redis.connect();
-
 redis.on("error", (err) => console.error("Redis Error: ", err));
+
+await redis.connect();
 
 export default redis;
